27468e59eb743d4309dd792f3eabdda6f34a06a9
SyahranFD <fadhilrafa1@gmail.com>
2025-02-13 10:54:21 +0700

    add api gateway url

--- routes/hike.js
+++ routes/hike.js
@@ -1,24 +1,34 @@
 const uuid = require('node-uuid');
-exports.index = function(req, res) {
-  res.app.get('connection').query( 'SELECT * FROM HIKES', function(err,
-rows) {
-    if (err) {
-      res.send(err);
-    } else {
-      console.log(JSON.stringify(rows));
-      res.render('hike', {title: 'My Hiking Log', hikes: rows});
-  }});
+const axios = require('axios');
+
+const API_GATEWAY_URL = process.env.API_GATEWAY_URL;
+
+exports.index = async function(req, res) {
+  try {
+    const response = await axios.get(`${API_GATEWAY_URL}/hikes`);
+    const hikes = response.data;
+    console.log(JSON.stringify(hikes));
+    res.render('hike', {title: 'My Hiking Log', hikes: hikes});
+  } catch (err) {
+    res.send(err);
+  }
 };
-exports.add_hike = function(req, res){
+
+exports.add_hike = async function(req, res){
   const input = req.body.hike;
-  const hike = { HIKE_DATE: new Date(), ID: uuid.v4(), NAME: input.NAME,
-  LOCATION: input.LOCATION, DISTANCE: input.DISTANCE, WEATHER: input.WEATHER};
+  const hike = { 
+    HIKE_DATE: new Date(), 
+    ID: uuid.v4(), 
+    NAME: input.NAME,
+    LOCATION: input.LOCATION, 
+    DISTANCE: input.DISTANCE, 
+    WEATHER: input.WEATHER
+  };
   console.log('Request to log hike:' + JSON.stringify(hike));
-  req.app.get('connection').query('INSERT INTO HIKES set ?', hike, function(err) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.redirect('/hikes');
-      }
-   });
+  try {
+    await axios.post(`${API_GATEWAY_URL}/hikes`, hike);
+    res.redirect('/hikes');
+  } catch (err) {
+    res.send(err);
+  }
 };
