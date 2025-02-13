8d32c40a3bd532f2ec3983aa9be57f42db043049
SyahranFD <fadhilrafa1@gmail.com>
2025-02-13 12:51:42 +0700

    add axios in package.json

--- routes/hike.js
+++ routes/hike.js
@@ -5,7 +5,7 @@ const API_GATEWAY_URL = process.env.API_GATEWAY_URL;
 
 exports.index = async function(req, res) {
   try {
-    const response = await axios.get(`${API_GATEWAY_URL}/hikes`);
+    const response = await axios.get(`https://nhfi7qbnh4.execute-api.ap-southeast-1.amazonaws.com/act1/hikes`);
     const hikes = response.data;
     console.log(JSON.stringify(hikes));
     res.render('hike', {title: 'My Hiking Log', hikes: hikes});
@@ -26,7 +26,7 @@ exports.add_hike = async function(req, res){
   };
   console.log('Request to log hike:' + JSON.stringify(hike));
   try {
-    await axios.post(`${API_GATEWAY_URL}/hikes`, hike);
+    await axios.post(`https://nhfi7qbnh4.execute-api.ap-southeast-1.amazonaws.com/act1/hikes`, hike);
     res.redirect('/hikes');
   } catch (err) {
     res.send(err);
